0be74443f5bc845345f1d1158e6ad3ddd5350805
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-07-14 10:19:00 -0700

    allow the application to accept input data

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+//BUSINESS logic
+
+
+
+
+
+
+
+
+//UI LOGIC
+$(document).ready(function () {
+  $("#ping-pong").submit(function (event) {
+    event.preventDefault();
+    var userInput = $("number").val();
+  });
+})
