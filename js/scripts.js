bd38b52f624c73dcb05c262acfb99012e24ec042
Kheifer Fuller <kheifer.fuller@gmail.com>
2017-07-14 13:53:29 -0700

    add optimization to js and index

--- js/scripts.js
+++ js/scripts.js
@@ -1,29 +1,21 @@
-//BUSINESS logic
-function pingPongGame(userInput) {
-  answer =[];
-  for(var i=1; i<=userInput; i++){
-    //answer.push(i);
-    if (i%15 === 0) {
-      answer.push("<li>pingpong</li>");
-    } else if (i%5 === 0) {
-      answer.push("<li>pong</li>");
-    } else if (i%3 === 0) {
-      answer.push("<li>ping</li>");
+// BUSINESS logic
+function pingPongGame (userInput) {
+  var answer = [];
+  for (var i = 1; i <= userInput; i++) {
+    if (i % 15 === 0) {
+      answer.push('<li>pingpong</li>');
+    } else if (i % 5 === 0) {
+      answer.push('<li>pong</li>');
+    } else if (i % 3 === 0) {
+      answer.push('<li>ping</li>');
     } else {
-       answer.push("<li>" +i+ "</li>");
+      answer.push('<li>' + i + '</li>');
     }
-  }
-
+  };
   return answer;
 };
 
-
-
-
-
-
-
-//UI LOGIC
+// UI LOGIC
 $(document).ready(function () {
   $("#ping-pong").submit(function (event) {
     event.preventDefault();
@@ -31,7 +23,5 @@ $(document).ready(function () {
     var pingNum = pingPongGame(userInput);
     $(".output").show();
     $("#results").html(pingNum);
-
-
   });
 });
